f0b090058626e416fe8436d14762189e3233da31
John Oscar D. Roble <roblejod@lic.net>
2024-03-01 15:17:36 +0800

    First commit

---
+++ admin.py
@@ -0,0 +1,99 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_mysqldb import MySQL
+from flask import jsonify
+
+app = Flask(__name__)
+
+# Set a secret key for the application when using 'flash'
+app.secret_key = 'qWer#123ty'
+
+app.config['MYSQL_HOST'] = '172.16.97.22'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ' '
+app.config['MYSQL_DB'] = 'records'
+
+mysql = MySQL(app)
+
+#admin login credentials
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "admin"
+
+# Define a list of routes that do not require authentication
+# You can add more routes if needed
+NO_AUTH_REQUIRED_ROUTES = ['admin_login', 'student_login']
+
+def convert_minutes_to_time(minutes):
+    hours = minutes // 60
+    mins = minutes % 60
+    return f"{hours:02d}:{mins:02d}:00"
+
+@app.route('/admin_login', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        username = request.form['uname']
+        password = request.form['pass']
+
+
+        if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+            session['authenticated'] = True
+            return redirect(url_for('manage'))
+        else:
+            return render_template('admin_login.html', message='Invalid Credentials')
+    return render_template('admin_login.html')
+
+@app.route('/logout')
+def logout():
+    # Clear the user's session
+    session.pop('authenticated', None)
+    return redirect(url_for('admin_login'))
+
+@app.route('/manage', methods=['GET', 'POST'])
+def manage():
+    if request.method == 'POST':
+        studentid = request.form['studentid']
+        name = request.form['name']
+        password = request.form['password']
+        course = request.form['course']
+
+        
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO students (studentid, name, password, course) VALUES (%s, %s, %s, %s)",
+                    (studentid, name, password, course))
+        mysql.connection.commit()
+        cur.close()
+
+        #return jsonify({"message": "Student registered successfully"})
+        # Use flash to store a message for the next request
+        flash("Student registered successfully")
+
+        # Redirect to the 'manage' route (GET) after successful registration
+        return redirect(url_for('manage'))
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT studentid, name, course, time_left FROM students")
+    students = cur.fetchall()
+    cur.close()
+
+    students = [(student[0], student[1], student[2], convert_minutes_to_time(student[3])) for student in students]
+
+    return render_template('manage.html', students=students)
+
+
+@app.route('/login')
+def student_login():
+    return render_template('student_login.html')
+
+@app.before_request
+def require_auth():
+    # Check if the user is trying to access a route that does not require authentication
+    if request.endpoint in ['admin_login']:
+        return
+
+    # Check if the user is authenticated
+    if not session.get('authenticated'):
+        # If not authenticated, redirect to the admin login page
+        return redirect(url_for('admin_login'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ logintest.py
@@ -0,0 +1,246 @@
+import tkinter as tk
+from datetime import datetime
+from tkinter import messagebox
+import time
+import mysql.connector
+from PIL import ImageTk, Image
+
+def get_usertype(student_id):
+    connection = connect_to_mysql()
+
+    if not connection:
+        return None
+    
+    cursor = connection.cursor()
+
+    query = "SELECT user_type FROM students WHERE studentid = %s"
+    cursor.execute(query, (student_id,))
+    result = cursor.fetchone()
+
+    cursor.close()
+    connection.close()
+    
+    if result:
+        return result[0]
+    else:
+        return None 
+
+# Initialize mysql
+def connect_to_mysql():
+    try:
+        # Replace these with your MySQL server details
+        connection = mysql.connector.connect(
+            host="172.16.97.22",
+            user="root",
+            password=" ",
+            database="records"
+        )
+        return connection
+    except mysql.connector.Error as err:
+        messagebox.showerror("Error", f"Error: {err}")
+        return None
+
+def on_closing():
+    messagebox.showinfo("Warning", "Cannot close the application this way.")
+
+def authenticate_user(student_id, password):
+    connection = connect_to_mysql()
+    if not connection:
+        return False
+    
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM students WHERE studentid = %s AND password = %s"
+    cursor.execute(query, (student_id, password))
+    result = cursor.fetchone()
+
+    cursor.close()
+    connection.close()
+
+    return result 
+
+def login():
+    student_id = student_id_entry.get()
+    password = password_entry.get()
+
+    usertype = get_usertype(student_id)
+
+    global current_session
+    if usertype == 'librarian':
+        login_window.withdraw()
+        librarian_window()
+    
+    if usertype == 'student':
+        if authenticate_user(student_id, password):
+
+            connection = connect_to_mysql()
+            if connection:
+                cursor = connection.cursor()
+
+                # Retrieve time_left for the student
+                select_query = "SELECT time_left FROM students WHERE studentid = %s"
+                cursor.execute(select_query, (student_id,))
+                result = cursor.fetchone()
+
+                if result and result[0] > 0:  # Assuming result[0] is the time_left column
+                    login_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    insert_query = "INSERT INTO login_sessions (studentid, login_time) VALUES (%s, %s)"
+
+                    try:
+                        cursor.execute(insert_query, (student_id, login_time))
+                        connection.commit()
+                        print("Login session successful")
+                    except mysql.connector.Error as insert_err:
+                        print(f"Error inserting login session: {insert_err}")
+
+                    
+
+                    # Store current session information
+                    current_session = {"student_id": student_id, "login_time": login_time}
+
+                    login_window.withdraw()
+                    main_window(student_id, login_time)
+                elif result and result[0] <= 0:
+                    error_label.config(text="You've already consumed your 10 hours this semester.", fg="red")
+                else:
+                    error_label.config(text="Invalid credentials", fg="red")
+
+                connection.close()  # Close the connection after using it
+
+def on_logout(window, student_id, login_time):
+    logout_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    connection = connect_to_mysql()
+    if connection:
+        cursor = connection.cursor()
+
+        update_query = "UPDATE login_sessions SET logout_time = %s WHERE studentid = %s AND login_time = %s"
+        cursor.execute(update_query, (logout_time, student_id, login_time))
+
+        update_time = "UPDATE login_sessions SET consumed_time = TIMESTAMPDIFF(MINUTE, login_time, logout_time) WHERE studentid = %s AND login_time = %s"
+        cursor.execute(update_time, (student_id, login_time))
+     
+        update_timeleft = "UPDATE students SET time_left = time_left - (SELECT consumed_time FROM login_sessions WHERE students.studentid = login_sessions.studentid AND students.studentid = %s ORDER BY login_time DESC LIMIT 1) WHERE students.studentid = %s;"
+        cursor.execute(update_timeleft, (student_id, student_id))
+
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        window.destroy()  # Corrected to use main_window window object 
+        student_id_entry.delete(0, 'end')
+        password_entry.delete(0, 'end')
+        login_window.deiconify()
+        
+
+def main_window(student_id, login_time):
+    global start_time
+    start_time = time.time()
+
+    window = tk.Tk()  # Changed variable name to window
+    window.title("Logged In")
+    window.geometry("300x150")
+
+    # Display current time at login
+    current_time = datetime.now().strftime("%H:%M:%S %p")
+    logged_in_label = tk.Label(window, text=f"Logged In: {current_time}")
+    logged_in_label.pack()
+
+    time_label = tk.Label(window, text="Running Time: 00:00:00")
+    time_label.pack()
+
+    def update_time():
+        elapsed_time = time.time() - start_time
+        running_time = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
+        time_label.config(text=f"Running Time: {running_time}")
+        window.after(1000, update_time)  # Update every second
+
+    update_time()
+
+    logout_button = tk.Button(window, text="Logout", command=lambda: on_logout(window, student_id, login_time))
+    logout_button.pack()
+
+    window.mainloop()
+
+def librarian_window():
+    window = tk.Tk()
+    window.title("Logged In")
+    window.geometry("300x150")
+
+    logout_button = tk.Button(window, text="Logout", command=window.destroy)
+    logout_button.pack()
+
+    window.mainloop() 
+
+
+# Login window
+global login_window
+login_window = tk.Tk()
+login_window.attributes("-fullscreen", True)
+
+# Disable Alt+Tab
+# login_window.attributes('-toolwindow', 1)
+# login_window.attributes('-topmost', 1)
+
+# Intercept the window close event
+# login_window.protocol("WM_DELETE_WINDOW", on_closing)
+
+# Open the image file
+image_path = "logo.png" 
+original_image = Image.open(image_path)
+resized_image = original_image.resize((100, 100)) 
+ 
+# Create a Tkinter-compatible photo image
+photo = ImageTk.PhotoImage(resized_image)
+
+#Create a frame to hold the image and text
+frame = tk.Frame(login_window)
+frame.pack(pady=(100, 10))
+
+label_image = tk.Label(frame, image=photo)
+label_image.pack(side=tk.LEFT)
+
+# Add the text "LIC Gateway" beside the image
+font_style = ("Arial", 30, "bold")
+label_text = tk.Label(frame, text="LIC Gateway", font=font_style)
+label_text.pack(side=tk.LEFT, padx=10)
+
+# Create a label for displaying the image
+# label = tk.Label(login_window, image=photo)
+# label.pack(pady=10)
+
+#frame for student ID label and entry
+frame_student_id = tk.Frame(login_window)
+frame_student_id.pack(side=tk.TOP, pady=5)
+
+student_id_label = tk.Label(frame_student_id, text="Student ID:")
+student_id_label.pack(side=tk.LEFT)
+
+student_id_entry = tk.Entry(frame_student_id)
+student_id_entry.pack(side=tk.LEFT, padx=5)
+
+#frame for student PASSWORD label and entry
+frame_password = tk.Frame(login_window)
+frame_password.pack(side=tk.TOP, pady=5)
+
+password_label = tk.Label(frame_password, text="Password:")
+password_label.pack(side=tk.LEFT)
+
+password_entry = tk.Entry(frame_password, show="*")
+password_entry.pack(side=tk.LEFT, padx=5)
+
+login_button = tk.Button(login_window, text="Login", command=login)
+login_button.pack(side=tk.TOP, pady=20)
+
+error_label = tk.Label(login_window, text="")
+error_label.pack(side=tk.TOP, pady=10)
+
+# Center the widgets in the window
+for widget in login_window.winfo_children():
+    widget.pack_configure(anchor='n')
+
+# WIDGETS ^^
+login_window.mainloop()
+
+
