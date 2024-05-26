613ec0d86fc2a5ff0de5401b0d3f6dcb19e9743d
shadycodee <mikojohn158@gmail.com>
2024-05-26 17:28:46 +0800

    staff registrar

--- webapp/models.py
+++ webapp/models.py
@@ -3,4 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-   
+class Staff(models.Model):
+    name = models.CharField(max_length=50, default="")
+    username = models.CharField(max_length=50, default="")
+    password = models.CharField(max_length=128, null=True, default=None) 
+
+    def __str__(self):
+        return f"{self.name} {self.username}"
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from .models import Staff
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 
@@ -46,4 +48,16 @@ def adminHome(request):
     return render(request, 'admin_home.html')
 
 def manageStaff(request):
-    return render(request, 'manage_staff.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        hashed_password = make_password(password)
+
+        staff = Staff(name=name, username=username, password=hashed_password)
+        staff.save()
+
+    staffs = Staff.objects.all()
+
+    return render(request, 'manage_staff.html', {'staffs': staffs})
