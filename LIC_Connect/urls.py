1a4f38fea6ecdb505331ee8ac6340796b844773b
shadycodee <mikojohn158@gmail.com>
2024-05-30 09:52:47 +0800

    url analytics

--- LIC_Connect/urls.py
+++ LIC_Connect/urls.py
@@ -33,4 +33,5 @@ urlpatterns = [
     path('admin_settings/', views.adminSettings, name='admin_settings'),
     path('delete/<int:staff_id>/', views.deleteStaff, name='delete_staff'),
     path('delete_student/<str:studentID>/', views.deleteStudent, name='delete_student'),
+    path('analytics/', views.analytics, name='analytics'),
 ]
